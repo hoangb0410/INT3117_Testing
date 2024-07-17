486f60d3ffabb86b50984b95627fff454181a078
Hoàng Cao <hoangb0410@192.168.17.101>
2024-07-17 17:41:30 +0700

    correct logic

--- functionalTesting.java
+++ functionalTesting.java
@@ -1,10 +1,11 @@
 public class functionalTesting {
     public static void main(String[] args) {
+        // test case 1:
         System.out.println(Discount(9, 4000));
     }
 
     public static int Discount(int x, int y) {
-        if (x < 0 || y < 0 || x > y) {
+        if (x < 1 || y < 1 || x > y) {
             return -1;
         }
         if (x >= 10 && y >= 5000) {
